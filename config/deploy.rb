8acc74600a975325a26820eb7ab6b415991c8ad6
koizs <k64c27f972rgba@icloud.com>
2020-03-20 11:45:33 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['ssh -i Ralph3104_pem.pem'] 
+                  keys: ['~/.ssh/Ralph3104.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
