f2ae5b1722c36284fba17d9b340bd458fbc14cc1
koizs <k64c27f972rgba@icloud.com>
2020-03-20 12:34:01 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  server '18.177.53.4', user: 'ec2-user', roles: %w{app db web}
+  
 
 end
